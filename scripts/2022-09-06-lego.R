39280673d33ab7c7191efdb60466a9d94428f243
MiqG <miquelangladagirotto@gmail.com>
2022-09-09 22:12:44 +0200

    finished lego dataset

--- scripts/2022-09-06-lego.R
+++ scripts/2022-09-06-lego.R
@@ -1,9 +1,10 @@
 require(tidyverse)
 require(ggpubr)
 require(extrafont)
-require(oblicubes)
-require(bittermelon)
 require(brickr)
+require(ggtext)
+require(grid)
+require(gridtext)
 
 # https://github.com/rfordatascience/tidytuesday/tree/master/data/2022/2022-09-06
 
@@ -13,12 +14,13 @@ plot_file = file.path(ROOT,"plots","2022-09-06-lego.png")
 
 # formatting
 FONT_SIZE = 12
-FONT_H1 = "Georgia"
-FONT_H2 = "Times New Roman"
+FONT_H1 = "Impact"
+FONT_H2 = "Georgia"#"Times New Roman"
 FONT_H3 = "Courier New"
 
-PAL_BACKGROUND = "#F6FFED"
-PAL_POINTS = "#ffba49"
+MATERIALS = c('Plastic','Rubber','Cloth','Metal','Cardboard/Paper','Foam')
+PAL_MATERIALS = setNames(get_palette("npg", length(MATERIALS)), MATERIALS)
+PAL_BACKGROUND = "khaki"
 
 # load data
 inventories = read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-09-06/inventories.csv.gz')
@@ -40,7 +42,8 @@ df = inventories %>%
     left_join(inventory_parts %>% dplyr::rename(inventory_img_url=img_url), by=c("id"="inventory_id")) %>%
     left_join(parts %>% dplyr::rename(part_name=name), by="part_num")
 
-df = df %>% 
+# how did the use of materials change along time?
+X = df %>% 
     count(year, part_material) %>%
     drop_na() %>% 
     group_by(part_material) %>%
@@ -50,8 +53,7 @@ df = df %>%
     mutate(rel_prop = prop/sum(prop)) %>%
     ungroup() 
 
-# how did the use of materials change along time?
-plt = df %>%
+plt = X %>%
     filter(year %in% seq(1950, 2022, 8)) %>%
     ggplot(aes(x=factor(year), y=rel_prop)) +
     geom_brick_col(aes(fill=part_material), position="stack", split_bricks=FALSE, label="LEGO") +
@@ -59,7 +61,76 @@ plt = df %>%
     fill_palette("npg") +
     theme_minimal()
 
-# what are the colors by material?
+# what are the colors diversity by material?
+X = df %>% 
+    distinct(year, part_material, color_id) %>% 
+    drop_na() %>% 
+    count(year, part_material)
+
+make_plot = function(X, material_oi) {
+    plt = X %>%
+        filter(year %in% seq(1950, 2022, 3) & part_material==material_oi) %>%
+        ggplot(aes(x=year, y=n)) +
+        geom_brick_col(
+            aes(fill=part_material), position="stack", 
+            split_bricks=FALSE, label="LEGO"
+        ) +
+        fill_palette(PAL_MATERIALS[material_oi]) +
+        labs(
+            x=NULL,
+            y=NULL,
+            fill=NULL
+        ) +
+        scale_x_continuous(limits = c(1950, 2022)) +
+        scale_y_continuous(breaks = seq(0,100,25), limits = c(0,110)) +
+        theme_pubr(x.text.angle = 45) +
+        theme(
+            aspect.ratio = 0.45,
+            
+            legend.position = c(0.01,1),
+            legend.justification = "left",
+            
+            axis.line = element_blank(),
+            axis.ticks.length=unit(.25, "cm"),
+
+            # plot
+            panel.background = element_rect(fill = PAL_BACKGROUND, colour=PAL_BACKGROUND),
+            plot.background = element_rect(fill = PAL_BACKGROUND, colour=PAL_BACKGROUND), 
+            plot.margin = unit(rep(0.3, times=4), "cm"),
+
+            # text
+            plot.title = element_blank(),
+            plot.subtitle = element_blank(),
+            plot.caption = element_blank(),
+            
+            legend.background = element_blank(),
+            legend.title = element_blank(),
+            legend.text = element_text(size=FONT_SIZE-1, family=FONT_H2),
+            axis.text = element_text(size=FONT_SIZE-1, family=FONT_H2)
+        )
+    
+    return(plt)
+}
+plts = lapply(
+    X %>% pull(part_material) %>% unique(),
+    function(material_oi){ make_plot(X, material_oi) }
+)
+fig = ggarrange(plotlist=plts, ncol=2, nrow=3) %>%
+    annotate_figure(
+        top = richtext_grob("Color diversity in *LEGO* materials", gp=gpar(fontsize=FONT_SIZE+20, fontfamily=FONT_H1), vjust=-0.1),
+        left = text_grob("N. Different Colors", size=FONT_SIZE-1, family=FONT_H2, rot=90, face="bold", vjust=0.5),
+        bottom = text_grob("Year", size=FONT_SIZE-1, family=FONT_H2, face="bold")
+    ) +
+    theme(
+        plot.background = element_rect(fill = PAL_BACKGROUND, colour=NA), 
+        plot.margin = margin(1, 1, 1, 1, "cm"),
+        panel.background = element_blank()
+    )
+
+ggsave(plot_file, fig, width=20, height=20, dpi=350, units="cm")
+
+print("Done!")
+
 
 # make lego logo
 font_file = system.file("fonts/spleen/spleen-8x16.hex.gz", package = "bittermelon")
@@ -136,9 +207,6 @@ plt = ggarrange(plt) +
     )
 
 # save
-ggsave(plot_file, plt, width=28, height=18, dpi=350, units="cm")
-
-print("Done!")
 
 
 # example
